bf810cbf686b95c948d3dd6164d6c7d2a6152cbe
KaiZeroSix <kai.tronsgard@students.mq.edu.au>
2025-05-09 14:49:07 +1000

    Week9
    Some issues with collision, but the code is correct. Issue with tile map collider

---
+++ Assets/Tiled/TileMapGame/WorkingTileMap.tsx
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="WorkingTileMap" tilewidth="16" tileheight="16" spacing="1" tilecount="132" columns="12">
+ <image source="tilemap.png" width="203" height="186"/>
+ <tile id="36">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="37">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+   <object id="2" x="0" y="0" width="16" height="16"/>
+   <object id="3" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="38">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+</tileset>
